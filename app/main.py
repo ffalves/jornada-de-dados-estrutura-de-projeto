d665bd9be15af2ce3cd4d8498dd3235cba2be60b
Fefe Alves <monitore.falves@gmail.com>
2024-11-19 22:54:50 -0300

    Add main.py

--- app/main.py
+++ app/main.py
@@ -1,38 +1,15 @@
-import os # working with files and folders
-import glob # list files
-import pandas as pd
-from typing import List
-import openpyxl
+from app.pipeline.extract import extract_from_excel
+from app.pipeline.transform import concatenate_dataframes
+from app.pipeline.load import save_xlsx
+from app.config import DATA_INPUT_PATH, DATA_OUTPUT_PATH
+import datetime
 
-
-path = 'data/input'
-
-"""
-Function to read files from data/input
-
-args: input_path {str}: path to the input folder
-
-return: a list of dataframes
-"""
-
-def extract_from_excel(input_path: str) -> List[pd.DataFrame]:
-    all_files = glob.glob(os.path.join(input_path, "*.xlsx"))
-
-    # Verifying files
-    if not all_files:
-        print(f'No files found in {input_path}')
-        return []
-
-    dataframe_list = []
-    for file in all_files:
-        print(f'Reading file {file}')
-        dataframe_list.append(pd.read_excel(file))
-    
-    return dataframe_list
-
-if __name__ == "__main__":
-    dataframe_list = extract_from_excel(path)
-    print(f'Total files: {len(dataframe_list)}')
-    for i, df in enumerate(dataframe_list):
-        print(f'File {i+1}: {df.shape[0]} rows, {df.shape[1]} colums')
-        
+if __name__ == '__main__':
+    # Extract
+    df_list = extract_from_excel(input_path = DATA_INPUT_PATH)
+    # Transform
+    df = concatenate_dataframes(df_list)
+    # Create filename with datetime
+    filename = f'output_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.xlsx'
+    # Load
+    save_xlsx(df, output_path = DATA_OUTPUT_PATH, filename=filename)
